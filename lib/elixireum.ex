4e577b7813334767116c6f2560a54185750c3efc
Maxim Filonov <53992153+sl1depengwyn@users.noreply.github.com>
2023-09-15 13:12:51 +0300

    Implement getting AST from a single file

--- lib/elixireum.ex
+++ lib/elixireum.ex
@@ -2,7 +2,6 @@ defmodule Elixireum do
   @moduledoc """
   Documentation for `Elixireum`.
   """
-
   @doc """
   Hello world.
 
@@ -13,6 +12,17 @@ defmodule Elixireum do
 
   """
   def hello do
-    :world
+
+    :erlang.put(:elixir_parser_columns, true)
+    :erlang.put(:elixir_token_metadata, true)
+    :erlang.put(:elixir_literal_encoder, false)
+
+    "./lib/test.ex"
+    |> File.read!()
+    |> String.to_charlist()
+    |> :elixir_tokenizer.tokenize(0, 0, [])
+    |> elem(4)
+    |> :elixir_parser.parse()
+    |> dbg()
   end
 end
