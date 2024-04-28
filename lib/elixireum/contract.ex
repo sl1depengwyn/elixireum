f448e0fc8d3deef47f4d1848ac55e28e1c277c76
Maxim Filonov <53992153+sl1depengwyn@users.noreply.github.com>
2024-04-28 17:16:53 +0300

    Start events

--- lib/elixireum/contract.ex
+++ lib/elixireum/contract.ex
@@ -1,13 +1,14 @@
 defmodule Elixireum.Contract do
-  alias Elixireum.{Functions, Variable}
+  alias Elixireum.{Event, Functions, Variable}
 
   @type t :: %__MODULE__{
           functions: Functions.t(),
           name: String.t(),
           private_functions: Functions.t(),
           variables: %{atom() => Variable.t()},
+          events: %{atom() => Event.t()},
           aliases: %{atom() => list()}
         }
 
-  defstruct [:functions, :name, :private_functions, :variables, :aliases]
+  defstruct [:functions, :name, :private_functions, :variables, :events, :aliases]
 end
